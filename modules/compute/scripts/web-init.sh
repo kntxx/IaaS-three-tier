7bf0b6f320cc8f5ab60ca10bc894f5eeb67ab563
kentatixx <kentatixx@gmail.com>
2026-07-17 12:55:16 +0800

    changing web server

--- modules/compute/scripts/web-init.sh
+++ modules/compute/scripts/web-init.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 apt-get update
-apt-get install -y python3-pip
-pip3 install flask
+apt-get install -y nginx
 
-cat << 'PYTHON' > /home/linuxadmin/web_frontend.py
-import urllib.request
-from flask import Flask
+cat << 'EOF' > /etc/nginx/sites-available/default
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-app = Flask(__name__)
+    location / {
+        proxy_pass http://${internal_lb_ip}:8080;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+    }
+}
+EOF
 
-@app.route('/')
-def index():
-    try:
-        
-        req = urllib.request.urlopen('http://${internal_lb_ip}:8080/')
-        api_response = req.read().decode('utf-8')
-        return f"<h1>Web Tier Online!</h1><h3>App Tier Response:</h3><pre>{api_response}</pre>"
-    except Exception as e:
-        return f"<h1>Web Tier Online!</h1><p>Error connecting to App Tier: {e}</p>"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
-PYTHON
-
-nohup sudo python3 /home/linuxadmin/web_frontend.py > /home/linuxadmin/web.log 2>&1 &
+systemctl restart nginx
