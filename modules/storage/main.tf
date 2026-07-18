33d4d0f6538ab1c2a9788942a010ed28f3cd7925
kentatixx <kentatixx@gmail.com>
2026-07-18 18:12:25 +0800

    off public storage

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -6,6 +6,7 @@ resource "azurerm_storage_account" "sa" {
     account_replication_type = "LRS"
     min_tls_version          = "TLS1_2"
     allow_nested_items_to_be_public = false
+    public_network_access_enabled = false
 }
 
 resource "azurerm_storage_container" "app_assets" {
